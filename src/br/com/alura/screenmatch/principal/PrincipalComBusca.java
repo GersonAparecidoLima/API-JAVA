4c3494d9e1733277d3cd6467685ebf7578c3796e
GersonAparecidoLima <gersonaparecido@gmail.com>
2026-02-15 16:27:36 -0300

    HttpClient

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalComBusca {
+
+    public static void main (String[] args) throws IOException, InterruptedException{
+     
+    	   HttpClient client = HttpClient.newHttpClient();
+    	   HttpRequest request = HttpRequest.newBuilder()
+    	         .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=c727e294"))
+    	         .build();
+
+    	// O próximo passo para ver o resultado da API:
+           HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+           
+           System.out.println(response.body());
+    	   
+    	   
+    }	
+	
+}
